cafcf69966c40f8a1c383838ee139321795cf927
Fernando Romero Gonzalez <fernanromeroglez@gmail.com>
2025-11-25 11:58:13 +0100

    examen.py

---
+++ examenpython.py
@@ -0,0 +1,17 @@
+numero = input('porfavor introduzca un numero mayor a 0')
+if numero == 0:
+    print ('finalizado')
+elif numero < 0:
+    print  ('numero invalido porfavor introduzca uno nuevo')
+elif numero > 0 :
+     numero = int(input('traduciendo en arte ASCII'))
+
+from pyfiglet 
+if numero > 0 :
+import figlet_format
+bucle for
+for numero in 0 
+print(figlet_format('Finalizado'))
+if numero > 0
+continue 
+print (numero)
