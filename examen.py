ab0eacaddb4624822eab99c9a8989efdb905301a
Fernando Romero Gonzalez <fernanromeroglez@gmail.com>
2025-11-25 11:20:29 +0100

    examen

---
+++ examen.py
@@ -0,0 +1,64 @@
+git config --global user.name Ridno003
+git config --global user.email fernanromeroglez@gmail.compile
+git config --global user.editor Code
+git config --global color.ui True
+git config --global --list
+
+git init webInstituto
+git add .
+git status
+git branch develop
+git status
+
+nano index.html
+git status
+git add .
+git commit -m index.html
+
+git branch -av develpo
+git remote add origin https://github.com/Ridno003/webInstituto.git
+git push -u origin develop
+
+git commit -am fernando
+git diff
+git restore 
+git commit fernando
+git push
+
+git config --local user.email
+mkdir correo@gmail.com
+git add .
+git commit
+git push 
+git rm contacto.html
+git restore contacto.html
+
+git config --global --list
+git show contacto.html
+git blame
+
+git config --list --show-origin 
+git branch -M develop
+git remote 
+add origin git remote -v
+git push
+
+git commit 
+git reset --soft head ~n
+git reset --mixed
+git reset --hard head ~n
+
+
+git init privado.txt
+git add privado.txt
+git rm privado.txt
+git revert privado.txt
+git init notas.docs
+git remove notas.docx
+git add notas.docs .gitignore
+
+git branch 
+git fetch 
+git commit 
+git push
+
